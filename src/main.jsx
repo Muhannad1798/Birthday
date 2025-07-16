20fa84fcbb5026b44f82bc97a92abcb13ba8a497
Muhannad1798 <Muhannad987m@gmail.com>
2025-07-16 18:18:22 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 
 createRoot(document.querySelector('#root')).render(<App />)
